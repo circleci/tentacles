ca5192c747a06be7cd38160c2bf18342b079863c
Matthew Boston <matthew@matthewboston.com>
2018-09-11 17:57:04 -0600

    dissoc :throw-exceptions from the body payload in mutating requests

--- src/tentacles/core.clj
+++ src/tentacles/core.clj
@@ -112,7 +112,7 @@
                         (when if-modified-since
                           {:headers {"if-Modified-Since" if-modified-since}}))
         raw-query (:raw query)
-        proper-query (query-map (dissoc query :auth :oauth-token :all-pages :accept :user-agent :otp))
+        proper-query (query-map (dissoc query :auth :oauth-token :all-pages :accept :user-agent :otp :throw-exceptions))
         req (if (#{:post :put :delete} method)
               (assoc req :body (json/generate-string (or raw-query proper-query)))
               (assoc req :query-params proper-query))]
